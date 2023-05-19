0da3e21dfaf21b19bbdde9af6aaeff3ee0cca68c
José Augusto de Aquino <60511182+joseaugustoaquino@users.noreply.github.com>
2023-05-19 10:36:29 -0300

    feat: property of gifs

---
+++ lib/z_notification/z_alert_custom/z_alert_custom_gif.dart
@@ -0,0 +1,8 @@
+class ZAlertCustomGif {
+  static const String manGif = "assets/gifs/man.gif";
+  static const String playGif = "assets/gifs/play.gif";
+  static const String mailGif = "assets/gifs/mail.gif";
+  static const String moveGif = "assets/gifs/move.gif";
+  static const String submitGif = "assets/gifs/submit.gif";
+  static const String shareGif = "assets/gifs/share.gif";
+}
